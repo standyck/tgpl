8e9465fa56a07993fd7f5eda76083124091111ab
robert.kuhar <robert.kuhar@climate.com>
2015-11-10 20:06:14 -0800

    dup3 done

---
+++ ch01/dup3.go
@@ -0,0 +1,30 @@
+// Dup3 prints the count and text of lines that appear more than once
+// in the input.  It reads the whole file into memory and works out from
+// there.
+// $go run dup3.go dup1-in.txt dup2-arg.txt
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+		}
+		for _, line := range strings.Split(string(data),"\n") {
+			counts[line]++
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
